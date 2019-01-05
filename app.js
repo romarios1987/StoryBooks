e26193032d9a1ee1de3df87d61e8c6db8d065534
romarios1987 <romios047@gmail.com>
2019-01-05 23:04:01 +0200

    using passport-google-oauth20

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
+// Passport Config
+require('./config/passport')(passport);
+
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -8,6 +15,9 @@ app.get('/', (req, res) => {
     res.send('It works');
 });
 
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb://Remi:remi1987@ds149754.mlab.com:49754/storybook',
+    googleClientID: '326814590763-v3tpnoua5t6lrlmljg3konc878hnn89h.apps.googleusercontent.com',
+    googleClientSecret: '-TB6FU9D0EDq2PvpLX-zMpkv'
+};
